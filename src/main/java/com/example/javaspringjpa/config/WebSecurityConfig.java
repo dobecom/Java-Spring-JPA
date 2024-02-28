2a2d7e34f23c1baa55ce0012d288f94301704f5a
steve <steve@steves-MacBook-Pro.local>
2024-02-29 00:33:16 +0900

    add: configure Spring Security

---
+++ src/main/java/com/example/javaspringjpa/config/WebSecurityConfig.java
@@ -0,0 +1,57 @@
+package com.example.javaspringjpa.config;
+
+import com.example.javaspringjpa.module.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebSecurityConfig {
+    private final UserService userService;
+
+    // Disable security for the Matchers
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/docs/**");
+    }
+
+    // Configure the security filter chain for the specific HTTP requests
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/user", "/login", "/signup")
+                        .permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+    // Configure the AuthenticationManager
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, UserService userService) throws Exception {
+        AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        builder.userDetailsService(userService)
+                .passwordEncoder(bCryptPasswordEncoder);
+        return builder.build();
+    }
+
+    // Password encoder
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
