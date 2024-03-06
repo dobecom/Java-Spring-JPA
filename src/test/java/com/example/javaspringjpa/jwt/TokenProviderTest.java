1c4177615ec3df0a57cf9e4ccb3485de4a3e145b
steve <steve@steves-MacBook-Pro.local>
2024-03-07 00:40:27 +0900

    add: JWT config, JWT test code

---
+++ src/test/java/com/example/javaspringjpa/jwt/TokenProviderTest.java
@@ -0,0 +1,117 @@
+package com.example.javaspringjpa.jwt;
+
+import com.example.javaspringjpa.config.jwt.JwtProperties;
+import com.example.javaspringjpa.config.jwt.TokenProvider;
+import com.example.javaspringjpa.entity.User;
+import com.example.javaspringjpa.module.user.repository.UserRepository;
+import io.jsonwebtoken.Jwts;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Date;
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+public class TokenProviderTest {
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    @DisplayName("Test : generateToken")
+    @Test
+    void generateToken() {
+        // given
+        User testUser = userRepository.save(User.builder()
+                .email("user@gmail.com")
+                .name("name")
+                .password("test")
+                .build());
+
+        // when
+        String token = tokenProvider.generateToken(testUser, jwtProperties.getExpiredAt());
+
+        // then
+        Long userId = Jwts.parser()
+                .setSigningKey(jwtProperties.getSecretKey())
+                .parseClaimsJws(token)
+                .getBody()
+                .get("id", Long.class);
+
+        assertThat(userId).isEqualTo(testUser.getId());
+    }
+
+    @DisplayName("Test : validToken - Expired")
+    @Test
+    void validTokenExpired() {
+        // given
+        String token = JwtFactory.builder()
+                .expiration(new Date(new Date().getTime() - 1000))
+                .build()
+                .createToken(jwtProperties);
+
+        // when
+        boolean result = tokenProvider.validToken(token);
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+    @DisplayName("Test : validToken - Success")
+    @Test
+    void validToken() {
+        // given
+        String token = JwtFactory.withDefaultValues().createToken(jwtProperties);
+
+        // when
+        boolean result = tokenProvider.validToken(token);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("Test : getAuthentication")
+    @Test
+    void getAuthentication() {
+        // given
+        String userEmail = "user@gmail.com";
+        String token = JwtFactory.builder()
+                .subject(userEmail)
+                .build()
+                .createToken(jwtProperties);
+
+        // when
+        Authentication authentication = tokenProvider.getAuthentication(token);
+
+        // then
+        assertThat(((UserDetails) authentication.getPrincipal())
+                .getUsername()).isEqualTo(userEmail);
+    }
+
+    @DisplayName("Test : getUserId")
+    @Test
+    void getUserId() {
+        // given
+        Long userId = 1L;
+        String token = JwtFactory.builder()
+                .claims(Map.of("id", userId))
+                .build()
+                .createToken(jwtProperties);
+
+        // when
+        Long result = tokenProvider.getUserId(token);
+
+        // then
+        assertThat(result).isEqualTo(userId);
+    }
+}
